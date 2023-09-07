02d9b75a1f25a658de2c35c2214ba36fcb3ee646
Vl-VSV <vvs_2015@icloud.com>
2023-09-07 16:35:15 +0300

    Initial Commit

---
+++ SIAOD/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SIAOD
+//
+//  Created by Vlad V on 07.09.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
